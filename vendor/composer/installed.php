9f3646d93ed6a23310c9411f035c2f87d5dbdfdf
Eyu-Tes <eyoabtesfaye10@gmail.com>
2021-06-21 09:15:24 +0300

    Autoloading with composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'eutes/task-mgr',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eutes/task-mgr' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
